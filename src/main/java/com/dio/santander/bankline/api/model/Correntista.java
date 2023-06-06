0699d210c9fb07654542c6ea6dd9cb618b69d765
Igor Eduardo da Silva <igoreduardo20101@gmail.com>
2023-06-06 12:27:46 -0300

    Initial commit

---
+++ src/main/java/com/dio/santander/bankline/api/model/Correntista.java
@@ -0,0 +1,57 @@
+package com.dio.santander.bankline.api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tab_correntista")
+public class Correntista {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  @Column(length = 20)
+  private String cpf;
+  @Column(length = 60)
+  private String nome;
+
+  @Embedded
+  private Conta conta;
+
+  public Conta getConta() {
+    return conta;
+  }
+
+  public void setConta(Conta conta) {
+    this.conta = conta;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+}
